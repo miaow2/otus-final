a5e419ba26fa163409fddaee67372f09b447d365
Artem Kotik <miaow2@yandex.ru>
2020-11-12 04:05:04 +0300

    Сделан каркас главного компонента

--- netwatcher/frontend/src/components/app/app.js
+++ netwatcher/frontend/src/components/app/app.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from '../header';
+import Home from '../home';
+import NetWatcherService from '../../services/netwatcher-api';
+import { NetWatcherServiceProvider } from '../netwatcher-service-context';
 
 const App = () => {
+
+  const netwatcherService = new NetWatcherService()
+
   return (
-    <div>NetWatcher</div>
+    // <Provider store={store}>
+    // <AlertProvider template={AlertTemplate} {...alertOptions}>
+    <NetWatcherServiceProvider value={netwatcherService}>
+      <Router>
+        <Header />
+        {/* <Alerts /> */}
+        <div className="container">
+          <Switch>
+            <Route path="/" component={Home} exact />
+            {/* <Route path="/depts/:id" render={({ match }) => {
+              const { id } = match.params
+              return <CoursePage courseId={id} />
+            }} />
+            <Route path="/groups/:id" render={({ match }) => {
+              const { id } = match.params
+              return <CoursePage courseId={id} />
+            }} /> */}
+            {/* <Route path="/login" component={Login} /> */}
+            {/* <PrivateRoute path="/profile" component={Profile} /> */}
+          </Switch>
+        </div>
+      </Router>
+    </NetWatcherServiceProvider>
+    //   </AlertProvider>
+    // </Provider>
   );
 };
 
